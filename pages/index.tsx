f053dd71b0571196eae144223f361f2c09aa4e68
S-A-073 <lina@snaps.com>
2022-03-14 09:47:30 +0900

    Initial Settiing: Install tailwindcss

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,12 @@
 import type { NextPage } from 'next'
 
 const Home: NextPage = () => {
-  return null;
-}
+  return (
+  <div className='bg-red-500'>
+    <h1 className='text-black-500'>
+      It works
+    </h1>
+  </div>)
+};
 
 export default Home
