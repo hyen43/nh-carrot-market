f16b28eb43eaa755ccf0e46034acfead196e0803
S-A-073 <lina@snaps.com>
2022-03-17 20:44:28 +0900

    DESIGN: tailwindcss로 예제 만들기(practice tailwindcss)

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,51 @@
-import type { NextPage } from 'next'
+import type { NextPage } from 'next';
+
 
 const Home: NextPage = () => {
-  return (
-  <div className='bg-red-500'>
-    <h1 className='text-black-500'>
-      It works
-    </h1>
+
+  return (<div className='bg-slate-400 py-20 px-20 flex flex-col space-y-5'>
+    <div className='bg-white p-6 rounded-3xl shadow-xl'>
+      <span className='font-semibold text-3xl'>Select Item</span>
+      <div className='flex justify-between my-2'>
+        <span className='text-gray-500'>Grey Chair</span>
+        <span className='font-semibold'>$19</span>
+      </div>
+      <div className='flex justify-between my-2'>
+        <span className='text-gray-500'>Total table</span>
+        <span className='font-semibold'>$80</span>
+      </div>
+      <div className='flex justify-between mt-2 pt-2 border-t-2 border-dashed'>
+        <span>Total</span>
+        <span className='font-semibold'>$99</span>
+      </div>
+      <div className='mt-5 bg-blue-500 text-white p-3 text-center rounded-xl w-1/2 mx-auto'>Checkout</div>
+    </div>
+    <div className='bg-white rounded-2xl overflow-hidden shadow-xl'>
+      <div className='bg-blue-500 p-6 pb-14'>
+        <span className='text-white text-2xl '>Profile</span>
+      </div>
+      <div className='rounded-3xl p-6 relative -top-5 bg-white'>
+        <div className='flex justify-between items-end relative -mt-16'>
+          <div className='flex flex-col items-center'>
+            <span className='text-sm text-gray-500'>Orders</span>
+            <span className='font-medium'>340</span>
+          </div>
+          <div className='h-24 w-24 bg-red-400 rounded-full'/>
+          <div className='flex flex-col items-center'>
+            <span className='text-sm text-gray-500'>Spent</span>
+            <span className='font-medium'>$2,310</span>
+          </div>
+        </div>
+        <div className='relative mt-5 flex flex-col items-center -mb-5'>
+            <span className='text-lg font-medium'>나현</span>
+            <span className='text-sm text-gray-500'>한국</span>
+          </div>
+      </div>
+
+
+    </div>
+    <div className='bg-white p-10 rounded-2xl shadow-xl'></div>
+  
   </div>)
 };
 
